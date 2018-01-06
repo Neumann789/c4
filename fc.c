7e3cc1b519067c475f05efa2f43482674cb380fb
root <root@localhost.localdomain>
2018-01-06 09:37:03 +0800

    commit fc

---
+++ fc.c
@@ -0,0 +1,9 @@
+/*create fc.c at 20180106*/
+#include <stdio.h>
+int main(int argc,char **argv){
+	int fd, bt, ty, poolsz, *idmain;
+	int *pc, *sp, *bp, a,cycle;
+	int i, *t;
+	printf("hello my fc!\n");
+	
+}
